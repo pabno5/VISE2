d53be25c89f4cf2535965696f5369eb767502151
Oscar_Cardozo <78360032+OscarCardozoDev@users.noreply.github.com>
2025-09-24 15:33:33 -0500

    Delete dist directory

--- dist/utils/beneficios.d.ts
+++
@@ -1,4 +0,0 @@
-declare class Descuentos {
-    getDescuento(card: string, payment: number, countryRecidence?: string, countryPayment?: string, day?: string): number;
-}
-export default Descuentos;
--- dist/utils/restricciones.d.ts
+++
@@ -1,5 +0,0 @@
-import { Persona } from "../vise.interface";
-declare class Restriciones {
-    static getRestricion(person: Persona): boolean;
-}
-export default Restriciones;
